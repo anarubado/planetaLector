eb11cae8705e229dc5439f38c44b2957d991497a
CarlaArias <carla@digitalhouse.com>
2020-06-18 23:42:06 -0300

    estilos productos y detalle

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -6,7 +6,7 @@ const productsController = {
     index: function(req, res){
         return res.render('products');
     },
-
+    
     detail: function(req, res){
         return res.render('detail');
     }
