7eb3b8f3507e4dc148964469eb9e9261a9ff4417
MartinDvorkin <martindvorkin10@gmail.com>
2020-06-27 08:22:11 -0300

    Registro funcionando correctamente

--- site/src/controllers/usersController.js
+++ site/src/controllers/usersController.js
@@ -1,67 +1,72 @@
-const jsonModel = require('../models/jsonModel');
-const usersModel = jsonModel('users.json');
-const productsModel = jsonModel('products.json');
-const fs = require('fs');
-const path = require('path');
-const bcryptjs = require('bcryptjs');
-const {validationResult} = require('express-validator')
-
-
+const jsonModel = require("../models/jsonModel");
+const usersModel = jsonModel("users.json");
+const productsModel = jsonModel("products.json");
+const fs = require("fs");
+const path = require("path");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const usersController = {
+  register: function (req, res) {
+    //let slidesProducts = productsModel.processSlideProducts(15, 3);
+    let harryPotter = productsModel.filterNProducts("Harry Potter", 10);
+    return res.render("register", { harryPotter });
+  },
 
-    register: function(req, res){
-        //let slidesProducts = productsModel.processSlideProducts(15, 3);
-        let harryPotter = productsModel.filterNProducts("Harry Potter", 10);
-        return res.render("register", {harryPotter});
-    },
-
-    save: function(req, res){
-        let errors = validationResult(req);
-        if(errors.isEmpty()){
-        // Encripto clave de usuarios
-        req.body.password = bcryptjs.hashSync(req.body.password, 10);
-        let user = {
-            id:"",
-            ...req.body,
-        }
-       /* usersModel.guardarUno(user);
+  save: function (req, res) {
+    let errors = validationResult(req);
+    
+    if (errors.isEmpty()) {
+      // Encripto clave de usuarios
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      let user = {
+        id: "",
+        ...req.body,
+      };
+      /* usersModel.guardarUno(user);
         return res.redirect('/');*/
-       // Leer JSON
-        let usuarios = fs.readFileSync(path.join(__dirname, '..', 'data', 'users.json'),'utf-8');
-        usuarios = JSON.parse(usuarios);
-        
-        
-    
-        // Agrego la data
-        usuarios = [...usuarios, user];
+      // Leer JSON
+      let usuarios = fs.readFileSync(
+        path.join(__dirname, "..", "data", "users.json"),
+        "utf-8"
+      );
+      usuarios = JSON.parse(usuarios);
 
-        // Guardo la data
-        usuarios = JSON.stringify(usuarios, null, " ");
-    
-        fs.writeFileSync(path.join(__dirname, '..', 'data', 'users.json'), usuarios);
-        
-        return res.redirect('/');
-    }else{
-        // En caso de haber error lo muestre por pantalla
-        return res.render('register', {errors: errors.mapped()})
+      // Agrego la data
+      usuarios = [...usuarios, user];
+
+      // Guardo la data
+      usuarios = JSON.stringify(usuarios, null, " ");
+
+      fs.writeFileSync(
+        path.join(__dirname, "..", "data", "users.json"),
+        usuarios
+      );
+
+      return res.redirect("/");
+    } else {
+      // En caso de haber error lo muestre por pantalla
+      let harryPotter = productsModel.filterNProducts("Harry Potter", 10);
+      return res.render("register", {
+        errors: errors.mapped(),
+        harryPotter,
+        old: req.body,
+      });
     }
+  },
 
-    },
+  login: function (req, res) {
+    //let slidesProducts = productsModel.processSlideProducts(15, 3);
+    let jkRowling = productsModel.filterNProducts("J. K. Rowling", 10);
+    return res.render("login", { jkRowling });
+  },
 
-
-    login: function(req, res){
-        //let slidesProducts = productsModel.processSlideProducts(15, 3);
-        let jkRowling = productsModel.filterNProducts("J. K. Rowling", 10);
-        return res.render("login", {jkRowling});
-    },
-
-    cart: function(req, res){
-        //let slidesProducts = productsModel.processSlideProducts(15, 3);
-        let isaacAsimov = productsModel.filterNProducts("Isaac Asimov", 10);
-        let cienciaFiccion = productsModel.filterNProducts("Ciencia ficción", 10)
-        return res.render("cart", {isaacAsimov, cienciaFiccion});
-    }
-}
+  cart: function (req, res) {
+    //let slidesProducts = productsModel.processSlideProducts(15, 3);
+    let isaacAsimov = productsModel.filterNProducts("Isaac Asimov", 10);
+    let cienciaFiccion = productsModel.filterNProducts("Ciencia ficción", 10);
+    return res.render("cart", { isaacAsimov, cienciaFiccion });
+  },
+};
 
 module.exports = usersController;
--- site/src/routes/users.js
+++ site/src/routes/users.js
@@ -1,17 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/usersController');
-const {body} = require('express-validator')
+const usersController = require("../controllers/usersController");
+const { body } = require("express-validator");
+const jsonModel = require("../models/jsonModel");
+const usersModel = jsonModel("users.json");
 
-router.get('/register', usersController.register);
-router.post('/register',[
-    body('email').notEmpty().withMessage('El campo de email es obligatorio').isEmail().withMessage('El campo debe ser un email'),
-    body('user').notEmpty().withMessage('El campo de usuario es obligatorio').isLength({min:3}).withMessage('El usuario debe tener minimo 3 caracteres'),
-    body('password').notEmpty().withMessage('El campo de contraseña es obligatorio').isLength({min:8}).withMessage('La contraseña debe tener minimo 8 caracteres'),
-], usersController.save);
+router.get("/register", usersController.register);
+router.post(
+  "/register",
+  [
+    body("email")
+      .notEmpty()
+      .withMessage("*Este Campo es obligatorio")
+      .isEmail()
+      .withMessage("El campo debe ser un email")
+      .custom((value) => {
+        let user = usersModel.findBySomething((user) => user.email == value);
 
-router.get('/login', usersController.login);
+        return !user;
+      })
+      .withMessage("Email ya registrado"),
+    body("user")
+      .notEmpty()
+      .withMessage("*Este Campo es obligatorio")
+      .isLength({ min: 3 })
+      .withMessage("El usuario debe tener minimo 3 caracteres")
+      .custom((value) => {
+        let user = usersModel.findBySomething((user) => user.user == value);
 
-router.get('/cart', usersController.cart);
+        return !user;
+      })
+      .withMessage("Usuario ya registrado"),
+    body("password")
+      .notEmpty()
+      .withMessage("*Este Campo es obligatorio")
+      .isLength({ min: 8 })
+      .withMessage("La contraseña debe tener minimo 8 caracteres"),
+  ],
+  usersController.save
+);
+
+router.get("/login", usersController.login);
+
+router.get("/cart", usersController.cart);
 
 module.exports = router;
