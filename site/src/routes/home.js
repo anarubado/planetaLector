5b6f792eb01ee42348200b8ba9766af8957dd1cf
anarubado <rubadoana@gmail.com>
2020-07-16 22:45:11 -0300

    implementacion de suscripcion a newletter

--- site/src/routes/home.js
+++ site/src/routes/home.js
@@ -4,5 +4,6 @@ const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.index);
 router.get('/search', homeController.search);
+router.post('/suscription', homeController.suscribe);
 
 module.exports = router;
