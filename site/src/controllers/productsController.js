09ad2991b1038afaba1f1851b6a79725792f4f8f
CarlaArias <carla@digitalhouse.com>
2020-08-05 09:52:14 -0300

    edit y detail

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -1,5 +1,8 @@
 const jsonModel = require('../models/jsonModel');
 const productsModel = jsonModel('products.json');
+const dbModel = require ('../models/dbModel');
+let db = require ('../database/models');
+let sequelize = db.sequelize
 
 const productsController = {
 
@@ -24,6 +27,25 @@ const productsController = {
         });
 
         return res.render('detail', {harryPotter, jkRowling, detail});
+    }, 
+    edit: function(req, res){
+        let pedidoProducto = db.Products.findByPk(req.params.idProduct, {include: {
+              all: true
+            }})
+        let pedidoCategory = db.Categories.findAll()
+        let pedidoSubCategory = db.SubCategories.findAll()
+      
+        Promise.all([pedidoProducto,pedidoCategory,pedidoSubCategory])
+            //console.log(product);
+            .then(function([product,category,subCategory]){
+                console.log(category);
+                return res.render('editarProducto',{product,category,subCategory})
+            })
+            
+    },
+    update: function() {
+        //let harryPotter = productsModel.filterNProducts("Harry Potter", 10);
+        //let jkRowling = productsModel.filterNProducts("J. K. Rowling", 10);
     }
 }
 
--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -4,5 +4,8 @@ const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.index);
 router.get('/detail/:idProduct', productsController.detail);
+/*Rutas para editar un producto*/
+router.get('/edit/:idProduct', productsController.edit);
+router.post('/edit/:idProduct', productsController.update);
 
 module.exports = router;
