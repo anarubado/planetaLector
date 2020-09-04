f3e8a82631f76635e5640c4675dc1d8600dd6d9c
MartinDvorkin <martindvorkin10@gmail.com>
2020-09-04 18:04:10 -0300

    solucionado sweet alert

--- site/public/js/sweetAlerts.js
+++ site/public/js/sweetAlerts.js
@@ -1,24 +1,28 @@
 window.addEventListener(
     "load", function(){
-        let trash = document.querySelector("#trash")
-        if (trash) {
-            trash.addEventListener("submit", function(e){
-                e.preventDefault()
-                Swal.fire({
-                    title: '¿Estás seguro?',
-                    text: "Deberás volver a realizar esta acción en caso de deshacerla",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Si!',
-                    cancelButtonText: 'Volver'
-                  }).then((result) => {
-                    if (result.value) {
-                      trash.submit()
-                    }
-                  })
-            })
-        }
+      if(document.querySelectorAll(".trash")){
+        let trash = document.querySelectorAll(".trash");
+        trash.forEach(function(item){
+         
+          item.addEventListener("submit", function(e){
+            e.preventDefault();
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: "Deberás volver a realizar esta acción en caso de deshacerla",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si!',
+                cancelButtonText: 'Volver'
+              }).then((result) => {
+                if (result.value) {
+                  this.submit()
+                }
+              })
+          })
+        })
+      }
+    
     }
 )
